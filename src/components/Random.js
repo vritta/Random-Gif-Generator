6c4910e422339e0078ca6a5fbe642706f2636ac4
vritta <anuhartripathi15@gmail.com>
2024-02-21 13:24:47 +0530

    second commit

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const Random = () => {
+    const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
     const [gif, setGif] = useState('');
+    
+    async function fetchData(){
+      const url = `https://api.giphy.com/v1/gifs/random?apikey=${API_KEY}`
+      const {data} = await axios.get(url);
+      const imageSource = data.data.images.downsized_large.url;
+      setGif(imageSource);
+      console.log(imageSource);
+    }
+
+    useEffect(()=>{
+      fetchData();
+    },[])
+
     function clickHandler(){
-        return;
+      fetchData();
     }
 
   return (
     <div className='bg-green-500 rounded-md w-1/2 border border-black flex flex-col items-center gap-y-10 mt-[15px]'>
       <h1 className='text-2xl font-bold underline uppercase'>A Random Gif</h1>
       <img src={gif} alt="" width="450"/>
-      <button onClick={clickHandler}>Generate</button>
+      <button onClick={clickHandler} className='w-10/12 bg-white text-lg py-2 opacity-[70%] rounded-md mb-[10px]'>
+        Generate
+      </button>
     </div>
   )
 }
